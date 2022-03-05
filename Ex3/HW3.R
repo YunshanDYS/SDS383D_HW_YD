f9a0b4146047bdf0f0f2371bc4ab988a080feba5
YunshanDYS <yunshan@utexas.edu>
2022-03-04 21:44:45 -0600

    Ex3

--- Ex3/HW3.R
+++ Ex3/HW3.R
@@ -42,6 +42,54 @@ hist(res,breaks=50,freq=FALSE)
 dev.off()
 plot(data$City_Market_Rent,res)
 
+## Gibbs
+library(MASS)
+M <- 5000 # num of iterations
+
+# inital
+h <- 1
+beta_samp <- matrix(NA,ncol=p,nrow=M)
+w_samp <- rep(NA,M)
+lambda_samp <- matrix(NA,ncol=n,nrow=M)
+beta <- beta_samp[1,] <- rep(1,p)
+w <- w_samp[1] <- 1
+lambda <- lambda_samp[1,] <- rep(1,n)
+for (m in 2:M){
+  lam_X <- sqrt(lambda)*X
+  Vbeta_inv <- t(lam_X)%*%lam_X + K
+  Vbeta <- solve(Vbeta_inv)
+  mubeta <- Vbeta%*%(t(lambda*X)%*%y + Km)
+  beta <- beta_samp[m,] <- mvrnorm(1,mubeta,Vbeta/w)
+  
+  d_w <- n+d
+  eta_w <- eta + sum(y^2*lambda) + sum(m^2*K_vec) - t(mubeta)%*%Vbeta_inv%*%mubeta
+  w <- w_samp[m] <- rgamma(1,d_w/2,eta_w/2)
+  
+  b_lam <- (h + w*(y-X%*%beta)^2)/2
+  lambda <- lambda_samp[m,] <- rgamma(n,(h+1)/2,b_lam)
+  lambda_samp[m,] <- lambda
+}
+
+burnin <- 1000
+beta_gibbs <- apply(beta_samp[burnin:M,],2,mean)
+res_gibbs <- y - X%*%beta_gibbs
+jpeg("res_gibbs.jpeg")
+hist(res,breaks=50,col=rgb(0,0,1,1/4),freq=FALSE)
+hist(res_gibbs,breaks=50,freq=FALSE,col=rgb(1,0,0,1/4),add=T)
+dev.off()
+
+beta_samp[burnin:M,]
+
+stud <- res_gibbs/sqrt(apply((w_samp*lambda_samp)[burnin:M,],2,mean))
+jpeg("stud.jpeg")
+hist(stud,breaks=30,freq=FALSE)
+dev.off()
+
+plot(data$City_Market_Rent,lamb_inv)
+lamb_inv <- 1/apply(lambda_samp[burnin:M,],2,mean)
+jpeg("lamb_inv.jpeg")
+hist(lamb_inv,breaks=30,freq=FALSE)
+dev.off()
 
 
 
