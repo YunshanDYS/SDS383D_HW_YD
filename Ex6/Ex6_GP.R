d27244717730d8f565340e9251533003a39f9920
YunshanDYS <yunshan@utexas.edu>
2022-04-18 17:40:54 -0500

    Ex6

--- Ex6/Ex6_GP.R
+++ Ex6/Ex6_GP.R
@@ -157,8 +157,99 @@ ggplot(df,
   ylim(-3,3)
 ggsave("figures/GP_tau2sq_seq_M52.png", width = 8, height = 6)
 
+### utilities data
+data <- read.csv("utilities.csv",header=T)
+y <- data$gasbill/data$billingdays
+x <- data$temp
+n <- length(y)
+
+tau2sq <- 1e-6
+tau1sq <- 1
+b <- 15
+sigma2 <- 0.61
+## Function to calculate C(x,x^*)
+C_tilde <- function(x, xstar, b, tau1sq, tau2sq){
+  eucDistsq = (x - xstar)^2
+  tau1sq*exp(-.5*eucDistsq/b^2) + tau2sq*(x == xstar)
+}
+
+C <- Exp2Sigma(x, b, tau1sq, tau2sq)
+Cinv <- solve(C + sigma2*diag(n))
+xstar_seq <- seq(10,80,length.out = 500)
+fhat <- var <- rep(0,500)
+for (i in 1:500){
+  xstar <- xstar_seq[i]
+  Ct <- C_tilde(x, xstar, b, tau1sq, tau2sq)
+  Cstar <- C_tilde(xstar, xstar, b, tau1sq, tau2sq)
+  fhat[i] <- Ct%*%Cinv%*%as.matrix(y,ncol=1)
+  var[i] <- Cstar - Ct%*%Cinv%*%matrix(Ct,ncol=1)
+}
+
+## make plots
+png("figures/CI_GP.png",width=600,height=600)
+plot(x,y,ylim=c(-1,10))
+lines(xstar_seq,fhat,col="red")
+for (i in 1:500){
+  segments(xstar_seq[i], fhat[i]-1.96*sqrt(var[i]), xstar_seq[i], fhat[i]+1.96*sqrt(var[i]),col=alpha("red",0.4))
+}
+dev.off()
+
+## Function to calculate the log marginal distribution
+logmar <- function(y, x, b, tau1sq, tau2sq){
+  C <- Exp2Sigma(x, b, tau1sq, tau2sq)
+  y_mat <- matrix(y,ncol=1)
+  lp <- -n/2*log(2*pi) - 1/2*log(det(C + sigma2*diag(n))) - 1/2*t(y_mat)%*%solve(C + sigma2*diag(n))%*%y_mat
+  return(lp)
+}
+
+tau1sq_seq <- seq(10,80,length.out = 100)
+b_seq <- seq(10,80,length.out = 100)
+grid <- expand.grid(tau1sq_seq,b_seq)
+lp <- rep(NA,nrow(grid))
+for (i in 1:nrow(grid)){
+  lp[i] <- logmar(y, x, b = grid[i,2], tau1sq = grid[i,1], tau2sq = 1e-6)
+}
+
+# plot heatmap
+df <- data.frame(tau1sq = grid[,1], b = grid[,2], lp = lp)
+ggplot(df, aes(tau1sq, b, fill= lp)) + 
+  geom_tile()
+ggsave("figures/heatmap.png",width=8,height=6)
+df[which.max(lp),]
+tau1sq_hat <- df[which.max(lp),1]
+b_hat <- df[which.max(lp),2]
 
 
+## Use the optimal parameters
+tau2sq <- 1e-6
+tau1sq <- tau1sq_hat
+b <- b_hat
+sigma2 <- 0.61
+## Function to calculate C(x,x^*)
+C_tilde <- function(x, xstar, b, tau1sq, tau2sq){
+  eucDistsq = (x - xstar)^2
+  tau1sq*exp(-.5*eucDistsq/b^2) + tau2sq*(x == xstar)
+}
 
+C <- Exp2Sigma(x, b, tau1sq, tau2sq)
+Cinv <- solve(C + sigma2*diag(n))
+xstar_seq <- seq(10,80,length.out = 500)
+fhat <- var <- rep(0,500)
+for (i in 1:500){
+  xstar <- xstar_seq[i]
+  Ct <- C_tilde(x, xstar, b, tau1sq, tau2sq)
+  Cstar <- C_tilde(xstar, xstar, b, tau1sq, tau2sq)
+  fhat[i] <- Ct%*%Cinv%*%as.matrix(y,ncol=1)
+  var[i] <- Cstar - Ct%*%Cinv%*%matrix(Ct,ncol=1)
+}
+
+## make plots
+png("figures/CI_GP_2.png",width=600,height=600)
+plot(x,y,ylim=c(-1,10))
+lines(xstar_seq,fhat,col="red")
+for (i in 1:500){
+  segments(xstar_seq[i], fhat[i]-1.96*sqrt(var[i]), xstar_seq[i], fhat[i]+1.96*sqrt(var[i]),col=alpha("red",0.4))
+}
+dev.off()
 
 
